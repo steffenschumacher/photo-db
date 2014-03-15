be40c2304c966dc0c04f6b9ac303290617035fbf
ssch <ssch@slt-vpn-62-242-222-68.eng.tdc.net>
2014-03-15 11:42:35 +0100

    Initial project import

---
+++ src/photodb/PhotoDb.java
@@ -0,0 +1,36 @@
+/*
+ *  
+ *  TDC A/S CONFIDENTIAL
+ *  __________________
+ *  
+ *   [2004] - [2013] TDC A/S, Operations department 
+ *   All Rights Reserved.
+ *  
+ *  NOTICE:  All information contained herein is, and remains
+ *  the property of TDC A/S and its suppliers, if any.
+ *  The intellectual and technical concepts contained herein are
+ *  proprietary to TDC A/S and its suppliers and may be covered
+ *  by Danish and Foreign Patents, patents in process, and are 
+ *  protected by trade secret or copyright law.
+ *  Dissemination of this information or reproduction of this 
+ *  material is strictly forbidden unless prior written 
+ *  permission is obtained from TDC A/S.
+ *  
+ */
+
+package photodb;
+
+/**
+ *
+ * @author Steffen Schumacher <steff@tdc.dk>
+ */
+public class PhotoDb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
